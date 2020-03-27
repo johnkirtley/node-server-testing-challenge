dd0490209cb1415d266ade79a2c1397262d73b8d
johnkirtley <kirtleyj16@gmail.com>
2020-03-26 19:48:42 -0700

    :white_check_mark: Added server tests
    Added server tests

---
+++ api/server.spec.js
@@ -0,0 +1,39 @@
+const request = require('supertest');
+const server = require('./server');
+
+describe('server.js', () => {
+	// describe('CREATE /api/players', () => {
+	// 	it('should return 200 ok', async () => {
+	// 		const response = await request(server)
+	// 			.post('/api/players')
+	// 			.send({
+	// 				name: 'Player 5',
+	// 				number: 45
+	// 			});
+	// 		expect(response.status).toBe(200);
+	// 	});
+
+	// 	it('should respond with JSON', async () => {
+	// 		const response = await request(server)
+	// 			.post('/api/players')
+	// 			.send({ name: 'Player 7', number: 23 });
+	// 		expect(response.type).toMatch(/json/i);
+	// 	});
+	// });
+
+	describe('DELETE /api/players/:id', () => {
+		it('should return 200 ok', async () => {
+			const response = await request(server)
+				.delete('/api/players/22')
+				.send({ id: 22 });
+			expect(response.status).toBe(200);
+		});
+
+		it('should return player deleted message', async () => {
+			const response = await request(server)
+				.delete('/api/players/22')
+				.send({ id: 22 });
+			expect(response.body.success).toBe('Player removed');
+		});
+	});
+});
